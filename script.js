80e49d11d2b856da99f5e7fbb2cd00af5b45e894
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-27 17:24:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+// Loop through numbers from 1 to 20
+for (var i = 1; i <= 20; i++) {
+    // Check for multiples of both three and five first
+    if (i % 3 === 0 && i % 5 === 0) {
+        alert("FizzBuzz");
+    }
+    // Check for multiples of three
+    else if (i % 3 === 0) {
+        alert("Fizz");
+    }
+    // Check for multiples of five
+    else if (i % 5 === 0) {
+        alert("Buzz");
+    }
+    // Print the number itself for other cases
+    else {
+        alert(i.toString());
+    }
+}
